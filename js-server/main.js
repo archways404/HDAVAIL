132de079a41dc04cd769cc5b50a5d759a4d6bdfb
archways <archways@gmx.us>
2024-09-03 13:53:21 +0200

    initial layout and configuration

---
+++ js-server/main.js
@@ -0,0 +1,64 @@
+const fastify = require('fastify');
+const cors = require('@fastify/cors');
+const cookie = require('@fastify/cookie');
+const jwt = require('@fastify/jwt');
+const logger = require('./logger');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '127.0.0.1';
+const CORS_ORIGIN = process.env.CORS_ORIGIN;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const app = fastify({
+	logger,
+});
+
+app.register(cookie);
+app.register(jwt, {
+	secret: JWT_SECRET,
+	cookie: {
+		cookieName: 'authToken',
+		signed: false,
+	},
+});
+
+app.register(cors, {
+	origin: CORS_ORIGIN,
+	credentials: true,
+	allowedHeaders: ['Content-Type', 'Authorization'],
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+});
+
+app.register(require('./connector'));
+app.register(require('./routes/authentication'));
+
+// Middleware
+app.decorate('verifyJWT', async function (request, reply) {
+	try {
+		await request.jwtVerify();
+	} catch (err) {
+		reply.send(err);
+	}
+});
+
+app.listen({ port: PORT, host: HOST }, async function (err, address) {
+	if (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
+});
+
+app.addHook('onReady', async () => {
+	const client = await app.pg.connect();
+	try {
+		const res = await client.query('SELECT NOW()');
+		app.log.info(`PostgreSQL connected: ${res.rows[0].now}`);
+	} catch (err) {
+		app.log.error('PostgreSQL connection error:', err);
+		throw new Error('PostgreSQL connection is not established');
+	} finally {
+		client.release();
+	}
+});
