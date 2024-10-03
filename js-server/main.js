573569d46e7907d251257089f5b550231a551362
archways404 <archways@gmx.us>
2024-10-03 12:21:42 +0200

    route for the new logic

--- js-server/main.js
+++ js-server/main.js
@@ -43,6 +43,7 @@ app.register(require('./connector'));
 // Routes
 app.register(require('./routes/authentication'));
 app.register(require('./routes/admin'));
+app.register(require('./routes/schedule'));
 app.register(require('./routes/status'));
 app.register(require('./routes/ical'), {
 	hook: 'preHandler',
