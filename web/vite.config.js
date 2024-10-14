c33a5daae44dab7e68b2aafabb39506c59b3862a
archways <archways@gmx.us>
2024-10-14 22:28:42 +0200

    test

--- web/vite.config.js
+++ web/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
 	},
 	server: {
 		port: 5173,
-		host: 'localhost',
+		host: '0.0.0.0',
 		https: {
 			key: fs.readFileSync('../certificates/server-key.pem'),
 			cert: fs.readFileSync('../certificates/server-cert.pem'),
