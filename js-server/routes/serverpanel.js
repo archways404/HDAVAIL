434e6fcb572cbf1977c96e683598b95c64e638a6
archways404 <archways@gmx.us>
2024-12-17 11:12:09 +0100

    Update serverpanel.js

--- js-server/routes/serverpanel.js
+++ js-server/routes/serverpanel.js
@@ -47,7 +47,7 @@ async function routes(fastify, options) {
 
 	fastify.get('/update-test', async (request, reply) => {
 		try {
-			return reply.send({ message: 'test success!' });
+			return reply.send({ message: 'finally!' });
 		} catch (error) {
 			console.error(error);
 			return reply.status(500).send({ message: 'Internal server error' });
