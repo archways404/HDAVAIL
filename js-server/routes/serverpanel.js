afa591b1b802e69a70661553d41001602f60df6d
archways404 <archways@gmx.us>
2024-12-17 11:05:23 +0100

    init

--- js-server/routes/serverpanel.js
+++ js-server/routes/serverpanel.js
@@ -47,7 +47,7 @@ async function routes(fastify, options) {
 
 	fastify.get('/update-test', async (request, reply) => {
 		try {
-			return reply.send({ message: 'new version!' });
+			return reply.send({ message: 'test success!' });
 		} catch (error) {
 			console.error(error);
 			return reply.status(500).send({ message: 'Internal server error' });
